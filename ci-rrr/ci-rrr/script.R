2cbfeab74806ba5357bf7468f8cfea616ab2e8cf
alebel <alebel@cooperinvestors.com>
2017-02-14 17:12:22 +1100

    wip

--- ci-rrr/ci-rrr/script.R
+++ ci-rrr/ci-rrr/script.R
@@ -1,13 +1,18 @@
-library(tidyverse)
-
 # Let's import some data
 df = read.csv("australian_males_31Dec2016.csv")
+df = read.csv("M://Information Technology//Quant//R//australian_males_31Dec2016.csv")
 
 
 # Let's look at the data
 df
+head(df, 10)
 
 # Let's chart the data
+plot(df$Foot_length_cm, df$Reading_grade)
+
+# I like to use charts from a package called ggplot2
+# (Talk about libraries...  Hadley Wickham...  tidy data)
+library(tidyverse)
 ggplot(df, aes(Foot_length_cm, Reading_grade)) + geom_point()
 
 # Line of best fit
@@ -28,15 +33,17 @@ arrange(df, DOB) # <-- Now let's try view the data
 # For ease of use, let's now add an age column to our original data frame.  Judging from the filename, it's probably safe
 # to assume that data is from 31 Dec 2016, so the age as of the time of the 
 df$Age = 2016 - year(df$DOB)
+head(df, 5)
 
 # And let's try graphing it again:
 ggplot(df, aes(Foot_length_cm, Reading_grade, color = Age)) + geom_point()
 
-# Hmm, it sees a smoothe number (Age) and tries to smoothe the color.  Let's turn age into labels so ggplot2 treats them like different categories:
+# Hmm, ggplot sees a smoothe number (Age) and tries to smoothe the color.
+# Let's turn age into labels so ggplot2 treats them like different categories:
 df$age_string = as.character(df$Age)
 ggplot(df, aes(Foot_length_cm, Reading_grade, color = age_string)) + geom_point()
 
-# Chart's looking a bit muddled.  Let's look at separate charts
+# Chart's looking a bit muddled.  Let's look at separate charts by facetting
 ggplot(df, aes(Foot_length_cm, Reading_grade, color = age_string)) + geom_point() + facet_wrap( ~ Age)
 
 # ...no need to colour anymore:
@@ -44,3 +51,6 @@ ggplot(df, aes(Foot_length_cm, Reading_grade)) + geom_point() + facet_wrap( ~ Ag
 
 # Can we still do lines of best fit on each age?
 ggplot(df, aes(Foot_length_cm, Reading_grade)) + geom_point() + facet_wrap( ~ Age) + geom_smooth(method = "lm")
+
+
+
